38a3ee228ecd13e17670efc8c90c928425c6e7bf
Samuel Jardim <samuel.jardim@unyleya.com.br>
2019-11-19 16:38:50 -0300

    Making matrix

---
+++ index.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+canvas.width = 36
+canvas.height = 36
+
+//data structure
+var matrix = Array(canvas.width).fill(0).map(x => Array(canvas.height).fill(0))
+
+const fireColorsPalette = [
+    [7, 7, 7],
+    [31, 7, 7],
+    [47, 15, 7],
+    [71, 15, 7],
+    [87, 23, 7],
+    [103, 31, 7],
+    [119, 31, 7],
+    [143, 39, 7],
+    [159, 47, 7],
+    [175, 63, 7],
+    [191, 71, 7],
+    [199, 71, 7],
+    [223, 79, 7],
+    [223, 87, 7],
+    [223, 87, 7],
+    [215, 95, 7],
+    [215, 95, 7],
+    [215, 103, 15],
+    [207, 111, 15],
+    [207, 119, 15],
+    [207, 127, 15],
+    [207, 135, 23],
+    [199, 135, 23],
+    [199, 143, 23],
+    [199, 151, 31],
+    [191, 159, 31],
+    [191, 159, 31],
+    [191, 167, 39],
+    [191, 167, 39],
+    [191, 175, 47],
+    [183, 175, 47],
+    [183, 183, 47],
+    [183, 183, 55],
+    [207, 207, 111],
+    [223, 223, 159],
+    [239, 239, 199],
+    [255, 255, 255]
+]
+
+//fire souce
+for (let index = 0; index < canvas.width; index++) {
+    matrix[35][index] = 36
+}
+
+
+for (let column = 0; column < canvas.width; column++) {
+    for (let row = 0; row < canvas.height; row++) {
+        debugger
+        ctx.beginPath();
+        ctx.fillStyle = 'rgb(' + fireColorsPalette[matrix[row][column]] + ')';
+        ctx.rect(row, column, 1, 1);
+        ctx.fill();
+        ctx.closePath();
+
+        //atualizar a matrix
+        matrix[row][column] = matrix[row][column+1] - 1 < 0 ? 0 : matrix[row][column+1] - 1
+
+    }    
+}
